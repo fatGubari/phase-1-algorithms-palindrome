b8f859ca2cbc2dae02b00e400aa3e9eb0cc11866
fatima abdullah <fatimajubary@gmail.com>
2023-01-30 13:08:25 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,13 +1,38 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  let palindrome = ""
+ 
+  for (let i = word.length-1; i >= 0; i--) { 
+    palindrome += word[i]
+  }
+    console.log(palindrome)
+  if (palindrome === word) {
+    return true
+  }else{
+    return false
+  } 
 }
 
 /* 
   Add your pseudocode here
+  input
+  funReverse(input)
+    for input.leangth ; i--
+      revInput += input
+
+    if revInput === input 
+      return ture
+    else
+      return fal
+
+  print(check(input,revInput))
 */
 
 /*
   Add written explanation of your solution here
+  input string
+  make a variable that will hold the reverse of the string
+  compare the revers string with the original string
 */
 
 // You can run `node index.js` to view these console logs
@@ -15,9 +40,7 @@ if (require.main === module) {
   // add your own custom tests in here
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
-
   console.log("");
-
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
 }
